523afd24882a98c1eb4078fb80b9430b230fb2c7
xiaoxustudio <1783558957@qq.com>
2025-03-22 20:49:10 +0800

    更新部署

--- docs/steam.md
+++ docs/steam.md
@@ -3,22 +3,4 @@ outline: deep
 title: Awesome Yami
 ---
 
-<script setup>
-import {ref} from "vue"
-import { useData } from 'vitepress'
-import alova from '../src/alova.ts'
-import accessGames from "../src/access.json";
-const info = ref(null)
-const id =  new URLSearchParams(location.search).get('id')
-alova.Get(`/stm/api/appdetails/?appids=${id}`).then(d=>{
-    const data = d[id]
-    if(data && data.success){
-        info.value= data.data
-    }
-}).catch(()=>{})
-</script>
-
-<Title v-if="!~accessGames.findIndex(v=>v.id === id) && info" :info="info" />
-<h1 v-else-if="id">该游戏可能不是yami 编辑器制作或者没有加入我们</h1>
-
-<h1 v-if="!id">可在左侧查看 yami 制作的游戏</h1>
+<Title />
--- src/Title.vue
+++ src/Title.vue
@@ -1,53 +1,71 @@
 <template>
-    <div :class="styles.Title">{{ info.name }}（{{ info.release_date.date }}）</div>
-    <div class="flex gap margin">
-        开发者：<el-tag type="primary" v-for="item of info.developers">{{ item }}</el-tag>
-    </div>
-    <div class="flex gap margin">
-        厂商：<el-tag type="primary" v-for="item of info.publishers">{{ item }}</el-tag>
-    </div>
-    <div class="flex gap margin">
-        价格：{{ info.price_overview.final_formatted }}
-    </div>
-    <div>部分成就：</div>
-    <div class="flex gap margin">
-        <el-space direction="vertical">
-            <el-space>
-                <el-text line-clamp="1" tag="b" :key="index + item.name"
-                    v-for="item, index of info.achievements.highlighted">{{ item.name
-                    }}</el-text>
+    <div v-if="isAccess && info" style="width: 100%;">
+        <div :class="styles.Title">{{ info.name }}（{{ info.release_date.date }}）</div>
+        <div class="flex gap margin">
+            开发者：<el-tag type="primary" v-for="item of info.developers">{{ item }}</el-tag>
+        </div>
+        <div class="flex gap margin">
+            厂商：<el-tag type="primary" v-for="item of info.publishers">{{ item }}</el-tag>
+        </div>
+        <div class="flex gap margin">
+            价格：{{ info.price_overview.final_formatted }}
+        </div>
+        <div>部分成就：</div>
+        <div class="flex gap margin">
+            <el-space direction="vertical">
+                <el-space>
+                    <el-text line-clamp="1" tag="b" :key="index + item.name"
+                        v-for="item, index of info.achievements.highlighted">{{ item.name
+                        }}</el-text>
+                </el-space>
             </el-space>
+        </div>
+
+        <!-- <div :class="styles.Thumbnail"><img :src="info && info.header_image" /></div> -->
+        <el-carousel indicator-position="outside">
+            <el-carousel-item v-for="item in info.screenshots" :key="item.id">
+                <img :src="item.path_full" alt="">
+            </el-carousel-item>
+        </el-carousel>
+
+        <div :class="styles.Title">描述</div>
+        <p>{{ info.short_description }}</p>
+
+
+        <!-- 关于 -->
+        <el-space direction="vertical">
+            <div v-html="info.about_the_game"></div>
         </el-space>
+
+        <!-- steam查看 -->
+        <div :class="styles.Title">查看</div>
+        <p><iframe :src="`https://store.steampowered.com/widget/${info.steam_appid}/`" frameborder="0" width="646"
+                height="190"></iframe>
+        </p>
     </div>
 
-    <!-- <div :class="styles.Thumbnail"><img :src="info && info.header_image" /></div> -->
-    <el-carousel indicator-position="outside">
-        <el-carousel-item v-for="item in info.screenshots" :key="item.id">
-            <img :src="item.path_full" alt="">
-        </el-carousel-item>
-    </el-carousel>
-
-    <div :class="styles.Title">描述</div>
-    <p>{{ info.short_description }}</p>
-
-
-    <!-- 关于 -->
-    <el-space direction="vertical">
-        <div v-html="info.about_the_game"></div>
-    </el-space>
-
-    <!-- steam查看 -->
-    <div :class="styles.Title">查看</div>
-    <p><iframe :src="`https://store.steampowered.com/widget/${info.steam_appid}/`" frameborder="0" width="646"
-            height="190"></iframe>
-    </p>
+    <el-text v-else-if="!id" size="large">可在左侧查看 yami 制作的游戏</el-text>
+    <el-text v-else="!isAccess" size="large" type="danger">该游戏可能不是yami 编辑器制作或者没有加入我们</el-text>
 </template>
 <script setup lang="ts">
+    import { ref, computed, onMounted } from "vue"
+    import { useData } from 'vitepress'
+    import alova from '../src/alova.ts'
+    import accessGames from "../src/access.json";
     import styles from "./Title.module.less"
     import "./index.css"
-    interface TitleProps {
-        info: any
-    }
-    const { info } = defineProps<TitleProps>()
-    console.log(info)
+    const info = ref(null)
+    const loc = typeof location !== 'undefined' ? location : '' // ssr
+    const id = new URLSearchParams(loc?.search).get('id')
+    const isAccess = computed(() => accessGames.findIndex(v => v.id === +id) !== -1)
+    onMounted(() => {
+        if (!isAccess.value) return
+        if (id)
+            alova.Get(`/stm/api/appdetails/?appids=${id}`).then(d => {
+                const data = d[id]
+                if (data && data.success) {
+                    info.value = data.data
+                }
+            })
+    })
 </script>
