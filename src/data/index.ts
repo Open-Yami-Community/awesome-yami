1557c1ad679280cc4d78ab096cd1ee9b8bf27219
xiaoxustudio <1783558957@qq.com>
2025-03-27 13:41:19 +0800

    添加末卡未来

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,9 +1,11 @@
 import Data2346410 from './2346410.json';
 import Data2450100 from './2450100.json';
 import Data3278480 from './3278480.json';
+import Data1779760 from "./1779760.json";
 
 export default {
 	2346410: Data2346410,
 	2450100: Data2450100,
-	3278480: Data3278480
+	3278480: Data3278480,
+    1779760: Data1779760,
 };
